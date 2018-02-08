674e9ce4b0a791256c041547a5b5dc3795f0b3b3
AidzPrehot <aprehatny@yahoo.com>
2018-02-08 08:07:37 -0800

    added introduction header, added css grid for projects list, added logo in top left added a couple more animations as well and some additional html structure

--- js/app.js
+++ js/app.js
@@ -1,33 +1,40 @@
 MorphSVGPlugin.convertToPath("#prestart");
 let gucci1 = document.getElementById("prestart");
+let gucci2 = document.querySelectorAll('#projectList')
 
-const tl = new TimelineLite({
-  "onComplete": transition
-});
 
-setTimeout(function() {gucci1.style.display = "block"}, 990)
-tl.to(gucci1, .1, {morphSVG:"#start"},"+=1")
+window.onload = introAnim;
+
+function introAnim() {
+  const tl = new TimelineLite({
+    "onComplete": transition
+  });
+
+  setTimeout(function() {gucci1.style.display = "block"}, 990)
+  tl.to(gucci1, .1, {morphSVG:"#start"},"+=1")
   .to(gucci1, .3, {morphSVG:"#middle"})
   .to(gucci1, .3, {morphSVG:"#bigger"})
   .to(gucci1, 1.7, {morphSVG:"#logo", ease: Elastic.easeOut.config(.25, 0.65)});
 
 
+  function transition() {
+    gucci1.classList.add('animated');
+    gucci1.classList.add('flipOutY');
+    document.getElementById('maincontent').style.display = "block";
+    document.getElementById('maincontent').classList.add('flipInY');
+    document.getElementById('socialmedia').style.display = "grid";
+    document.getElementById('introduce').style.display = "none";
+    socialMedia();
+  }
 
-function transition() {
-  gucci1.classList.add('animated');
-  gucci1.classList.add('flipOutY');
-  document.getElementById('maincontent').style.display = "block";
-  document.getElementById('maincontent').classList.add('flipInY');
-  document.getElementById('socialmedia').style.display = "grid";
-  document.getElementById('socialmedia').classList.add('fadeIn');
-  document.getElementById('introduce').style.display = "none";
 
-  let speed = 0.3;
-  let ease = Elastic.easeInOut.config(.1, 0.5);
-  let delayAnim = 0;
+  function socialMedia() {
+    let speed = 0.3;
+    let ease = Elastic.easeInOut.config(.1, 0.5);
+    let delayAnim = 0;
 
-  $('.social-icon').each(function() {
-    let tl = new TimelineLite({  }),
+    $('.social-icon').each(function() {
+      let tl = new TimelineLite({  }),
       $this = $(this),
       $round = $this.find('.si-round'),
       $splash = $this.find('.si-splash'),
@@ -35,12 +42,11 @@ function transition() {
       $gradient = $this.find('.si-gradient'),
       color = $round.css('fill');
 
-    // tl.pause(); // turn on
+      // tl.pause(); // turn on
 
-    $this.css('opacity', 1);
+      $this.css('opacity', 1);
 
-    // Morph and animate colors
-
+      // Morph and animate colors
       $round.attr("fill",'#ef3d62');
 
       tl.to($round, speed, {
@@ -52,30 +58,31 @@ function transition() {
       }, 0)
 
 
-    // Animate icon
-    tl.to($icon, speed + .1, {
-      css: {
-        scale: 1.2,
-        transformOrigin: "center center",
-      },
-      ease: Back.easeInOut.config(1)
-    }, 0);
+      // Animate icon
+      tl.to($icon, speed + .1, {
+        css: {
+          scale: 1.2,
+          transformOrigin: "center center",
+        },
+        ease: Back.easeInOut.config(1)
+      }, 0);
 
 
-    // On hover play timeline
-    $(this).hover(function() {
-      tl.play();
-    }, function() {
-      let currentTime = tl.time();
-      tl.reverse(currentTime);
+      // On hover play timeline
+      $(this).hover(function() {
+        tl.play();
+      }, function() {
+        let currentTime = tl.time();
+        tl.reverse(currentTime);
+      });
+
+      setTimeout(function() {
+        tl.play();
+      }, delayAnim);
+      setTimeout(function() {
+        tl.reverse();
+      }, delayAnim + 1000);
+      delayAnim = delayAnim + 100;
     });
-
-    setTimeout(function() {
-      tl.play();
-    }, delayAnim);
-    setTimeout(function() {
-      tl.reverse();
-    }, delayAnim + 1000);
-    delayAnim = delayAnim + 100;
-  });
+  }
 }
