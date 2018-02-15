07443b34693f972f5fe75096856462fb82420868
AidzPrehot <aprehatny@yahoo.com>
2018-02-14 22:52:28 -0800

    Added new gif images and added more content to China's page added svg fill colors to china, arcade, memory case study pages

--- js/app.js
+++ js/app.js
@@ -21,21 +21,22 @@ new Vue ({
     changeArcade: function() {
       this.title = 'Classic Arcade Game';
       this.subhead= 'Designed the minimal user interface and provided full functionality to the game using HTML, CSS and Javascript.';
-      this.playApp=  'Play App';
+      this.playApp=  "<a class='visitAll' id='visitArcade' class='animated fadeIn' target='_blank' href='https://enomer.github.io/Concentration-memory-game/'>Play App</a>";
       this.headerAssets= 'Placeholder';
       this.subHead1= 'Project Objectives';
-      this.bodyContent1= "<br>• Provide the player with movement using arrow keys and how far each arrow key takes the player character•Give enemies speed values that randomly reset on re entering frame per lane, give them collision detection as well<br>•make the game board a fixed area of which the player can navigate <br>•Complex applications such as enemy and player instance must be “broken down” into simpler entities that manage their own properties and functionality";
+      this.bodyContent1= "<br>• Provide the player with movement using arrow keys and how far each arrow key takes the player character<br> •Give enemies speed values that randomly reset on re entering frame per lane, give them collision detection as well<br> •Make the game board a fixed area of which the player can navigate <br> •Complex applications such as enemy and player instance must be “broken down” into simpler entities that manage their own properties and functionality";
       this.subHead2= 'Challenges';
       this.bodyContent2= "Main challenges of the game was understanding the game engine, some particular aspects of game functionality which were challenging include:<br><br>•	Handling how classes provide functionality and new instances specifying values such as coordinate position (the enemies) and what to include and what not to include in these classes<br>•	Player character update methods should distinguish between when the character has reached the edge of the game board and can not go any further from event listener and x/y coordinate information<br>•	Understanding the game engine and how it continuously updates then renders each frame<br>•	enemy collision detection and enemy speed randomization on reaching certain x coordinate";
       this.subHead3= 'Technologies';
       this.bodyContent3='HTML5 Canvas game Engine<br>Javascript ES2015/ES6<br>Web APIs i.e. Event Listeners<br> Object Oriented Programming/ ES6 Classes<br>SVG';
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      document.getElementById('port1').style.fill = "url(#gradArcade)"
     },
     changeMemory: function() {
       this.title = 'Interactive Memory Game';
       this.subhead= 'Designed the minimal user interface and provided full functionality to the game using HTML, CSS and Javascript.';
-      this.playApp=  "<a id='visit' class='animated fadeIn' target='_blank' href='https://enomer.github.io/Concentration-memory-game/'>Play App</a>";
+      this.playApp=  "<a class='visitAll' id='visitMem' class='animated fadeIn' target='_blank' href='https://enomer.github.io/Concentration-memory-game/'>Play App</a>";
       this.headerAssets= ""
       this.subHead1= 'Project Objectives';
       this.bodyContent1= "The project needed to have the following features:<br><br>• The player flips one card over to reveal its underlying symbol. <br>• The player then turns over a second card, trying to find the corresponding card with the same symbol. <br>• If the cards match, both cards stay flipped over. <br>• If the cards do not match, both cards are flipped face down.<br> <img id='truffleShuffle' src='/img/shuffleDeck.gif'><br>In the real life version of the game, a player will flip over the cards to locate the pairs which match, with this in mind, there are a few animation interactions that i needed to address: <br><br>• Flipping Cards<br>• What happens when cards match <br>• What happens when cards do not match <br>• When the game finishes<br><br><br>Additional functionality:<br>• Star Rating system<br>• Time elapsed recorded visually on first button click<br>• Replay button which reshuffles the game board and reset all game state<br><br><div id=\"imgBox\"><img class=\"flipExamp animated flipInY infinite\" src='/img/number5.gif'><img class='flipExamp animated rubberBand infinite' src='/img/number5.gif'> <img class='flipExamp animated shake infinite' src='/img/number5.gif'> </div>";
@@ -46,22 +47,24 @@ new Vue ({
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
       this.gradient= "<svg width='0px' height='0px' ><defs><linearGradient id='lgrad' x1='0%' y1='100%' x2='100%' y2='0%' ><stop offset='0%' style='stop-color:rgb(255,255,199);stop-opacity:1' /><stop offset='100%' style='stop-color:rgb(0,183,255);stop-opacity:1' /></linearGradient></defs><rect x='0' y='0' width='100%' height='100%' fill='url(#lgrad)'/></svg>"
+      document.getElementById('port1').style.fill = "url(#gradConcen)"
 
     },
     changeChina: function() {
       this.title = "China's Energy Initatives";
-      this.subhead= 'Infographical web page with interactive elements, My introduction to javascript, which features a web page built with jQuery and datavisu.al; a platform utilizing d3.js, to produce data heavy, interactive infographics (responsive WIP).';
-      this.playApp=  'Visit Website';
-      this.headerAssets= 'Placeholder';
+      this.subhead= 'Infographical web page with interactive elements, My introduction to javascript, which features a web page built with jQuery and datavisu.al; a platform utilizing d3.js, to produce data heavy, interactive infographics';
+      this.playApp=  "<a class='visitAll' id='visitChina' class='animated fadeIn' target='_blank' href='https://enomer.github.io/Energy-Initiatives/#'>Visit Site</a>";
+      this.headerAssets= "<iframe id='graphChina' src='https://s3.amazonaws.com/datavisual/embeds/235c5691f4.html' style='height:400.4953877690468px; max-width:606px; width:100%; border: 0px; overflow: hidden' scrolling='no'></iframe><script src='http://datavisu.al/js/lazyLoad.js#!dv-6881f01518669842192,235c5691f4.html'></script>";
       this.subHead1= 'Project Objectives';
-      this.bodyContent1= "Informative interactive websites was developed with only a few factors as priority•	Design animated infographics which could be interacted with in hover states <br>•	Use Jquery to develop small microanimations and interactions <br>•	Experiment with creative CSS button elements utilizing different element attributes for styling using bootstrap button glyphs. <br>•	Script small sequences which depended on certain variables(both buttons clicked spawns small paragraph) <br><br>";
+      this.bodyContent1= "Informative, informational, and interactive website:<br> <br> •	Design animated infographics which have minor hover state interaction <br> •	Use jQuery to develop small microinteractions and animation <br>•	Develop experimental CSS buttons with psuedo class elements and creative techniques for styling using bootstrap button glyphs. <br> • Script small sequences which depended on certain variables(both buttons clicked spawns small paragraph) <br><br>";
       this.subHead2= 'Challenges';
-      this.bodyContent2= "The main challenges for interface functionality and design:<br>•	Minimal interface providing information for only four categories<br>•	Having certain elements phase in on events <br>•	Designing creative micro interactions for each infographic and having it switch out dynamically depending on category selection<br>";
+      this.bodyContent2= "The main challenges for interface functionality and design:<br><br>•	Minimal interface providing information for only four categories<br>•	Having certain elements phase in on events <br>•	Designing creative micro interactions for each infographic and having it switch out dynamically depending on category selection<br><br><img id='chinaBtnGif' src='/img/buttonPopup.gif'><br><br><h3>Visual Design Documentation</h3><br>My Visual Design process; sketches, wireframes, ideas from inforgraphics project can be viewed <a target='_blank' href='https://www.dropbox.com/s/6g5adgw6tms9ldg/Chinas%20energy%20initiatives%20Version_HQP.pdf?dl=0'>here</a>";
       this.subHead3= 'Technologies';
       this.bodyContent3='Code technologies and skills I got involved while working on this project<br><br>Front-End<br>HTML5<br>CSS3<br>bootstrap<br>Javascript/jQuery<br>Datavisu.al/d3.js/Web APIs';
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
       this.gradient= "<svg xmlns='http://www.w3.org/2000/svg' width='0px' height='0px' ><defs> <linearGradient id='lgrad' x1='0%' y1='100%' x2='100%' y2='0%' > <stop offset='0%' style='stop-color:rgb(255,228,122);stop-opacity:1' /><stop offset='100%' style='stop-color:rgb(255,213,48);stop-opacity:1' /></linearGradient> </defs><rect x='0' y='0' width='100%' height='100%' fill='url(#lgrad)'/></svg>"
+      document.getElementById('port1').style.fill = "url(#gradChina)"
     },
     changeExercise: function() {
       this.title = "Exercise: Responsive Design";
@@ -107,9 +110,15 @@ new Vue ({
       },2500);
     },
     clearIt: function() {
-      clearInterval(this.anInterval)
+      clearInterval(this.anInterval);
+      document.getElementById('port1').style.fill = ""
+    },
+    chinaInterval: function() {
+        this.anInterval = setInterval(
+          function() {
+            document.getElementById('headerAssets').innerHTML = "<iframe id='graphChina' src='https://s3.amazonaws.com/datavisual/embeds/235c5691f4.html' style='height:400.4953877690468px; max-width:606px; width:100%; border: 0px; overflow: hidden' scrolling='no'></iframe><script src='http://datavisu.al/js/lazyLoad.js#!dv-6881f01518669842192,235c5691f4.html'></script>";
+          }, 2500)
     }
-
   }
 
 });
